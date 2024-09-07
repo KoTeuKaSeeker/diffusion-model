335a65beb68b2a8f1cdfc45f4130127169c9244a
KoTeuKaSeeker <emaillit8@gmail.com>
2024-09-08 02:45:36 +0400

    Added definition of diffuse model.

--- diffusion_model.py
+++ diffusion_model.py
@@ -1,10 +1,12 @@
 import os
+import math
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from typing import List
 
 import torch
+import torch.nn as nn
 from torch.nn import functional as F
 from torch.utils.data import Dataset, IterableDataset
 from torchvision import transforms
@@ -153,6 +155,83 @@ class NoiseScheduler(torch.nn.Module):
         sqrt_one_minus_alphas_cumprod_t = self.get_index_from_list(self.sqrt_one_minus_alphas_cumprod, t, x_0.shape)
         return sqrt_alphas_cumprod_t * x_0 + sqrt_one_minus_alphas_cumprod_t * noise, noise
 
+
+class Block(nn.Module):
+    def __init__(self, in_ch, out_ch, time_emb_dim, up=False):
+        super().__init__()
+        self.time_mlp = nn.Linear(time_emb_dim, out_ch)
+        if up:
+            self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
+            self.transform = nn.ConvTranspose2d(out_ch, out_ch, 4, 2, 1)
+        else:
+            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=3)
+        self.bnorm = nn.BatchNorm2d(out_ch)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x, t):
+        h = self.bnorm(self.relu(self.conv1(x)))
+        time_emb = self.relu(self.time_mlp(t))
+        time_emb = time_emb[(..., ) + (None, ) * 2]
+        h = h + time_emb
+        h = self.bnorm(self.relu(self.conv2(h)))
+        return self.transform(h)
+
+class SinusidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+    
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+
+
+class SimpleUnet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        image_channels = 3
+        down_channels = (64, 128, 256, 512, 1024)
+        up_channels = (1024, 512, 256, 128, 64)
+        out_dim = 1
+        time_emb_dim = 32
+        
+        # Time embedding
+        self.time_mlp = nn.Sequential(
+            SinusidalPositionEmbeddings(time_emb_dim),
+            nn.Linear(time_emb_dim, time_emb_dim),
+            nn.ReLu()
+        )
+
+        # Initial projection
+        self.conv0 = nn.Conv2d(image_channels, down_channels[0], 3, padding=1)
+
+        # Downsample
+        self.downs = nn.ModuleList([Block(down_channels[i], down_channels[i+1], time_emb_dim) for i in range(len(down_channels) - 1)])
+
+        # Upsample
+        self.ups = nn.ModuleList([Block(up_channels[i], up_channels[i+1], time_emb_dim, up=True) for i in range(len(up_channels) - 1)])
+
+        self.output = nn.Conv2d(up_channels[-1], 3, out_dim)
+
+    def forward(self, x, timestep):
+        t = self.time_mlp(timestep)
+        x = self.conv0(x)
+        residual_inputs = []
+        for down in self.downs:
+            x = down(x, t)
+            residual_inputs.append(x)
+        for up in self.ups:
+            residual_x = residual_inputs.pop()
+            x = torch.cat((x, residual_x), dim=1)
+            x = up(x, t)
+        return self.output(x)
 
 def run(device_manager: DeviceManager, train_parameters: TrainParameters):
     noise_scheduler = NoiseScheduler(train_parameters.T, device_manager)
